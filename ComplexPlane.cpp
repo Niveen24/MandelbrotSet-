26e53619e1763de8b46b9c8ee0a851db6c4e830d
Niveen24 <niveensaeed06@gmail.com>
2025-11-29 20:49:53 -0800

    trying different color schemes, option 3

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -170,21 +170,17 @@ void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
 	{
 		float t = float(count) / float(MAX_ITER);
 
-				//option1:
-		r = Uint8(255 * pow(t, 0.6f));       // pink
-		g = Uint8(200 * pow(t, 1.5f));       // aqua/teal
-		b = Uint8(255 * pow(1 - t, 2.5f));   // fading cotton glow
 				//option 2:
-		//float red = 255.0f * (0.9f * (1.0f - t) + 0.3f * t);
-		//float green = 255.0f * (0.2f + 0.5f * t * t);
-		//float blue = 255.0f * (0.4f + 0.6f * t);
-		//float glow = 60.0f * (1.0f - fabs(0.5f - t) * 2.0f);
-		//red = min(255.0f, red + glow);
-		//green = min(255.0f, green + glow);
-		//blue = min(255.0f, blue + glow);
-		//r = Uint8(red);
-		//g = Uint8(green);
-		//b = Uint8(blue);
+		float red = 255.0f * (0.9f * (1.0f - t) + 0.3f * t);
+		float green = 255.0f * (0.2f + 0.5f * t * t);
+		float blue = 255.0f * (0.4f + 0.6f * t);
+		float glow = 60.0f * (1.0f - fabs(0.5f - t) * 2.0f);
+		red = min(255.0f, red + glow);
+		green = min(255.0f, green + glow);
+		blue = min(255.0f, blue + glow);
+		r = Uint8(red);
+		g = Uint8(green);
+		b = Uint8(blue);
 			//op 3:
 		//r = Uint8(180 * t * t);             // purple-pink highlight
 		//g = Uint8(20 * pow(1 - t, 3));     // dark shadow glow
